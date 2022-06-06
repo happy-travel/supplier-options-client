8147361716c7b5931010ad96176083f8aac069d7
Konstantin Yefremov <konstyefremov@gmail.com>
2022-06-06 15:06:37 +0400

    Remove IsEnabled property and Activate/Deactivate methods

--- HappyTravel.SupplierOptionsClient/Models/SlimSupplier.cs
+++ HappyTravel.SupplierOptionsClient/Models/SlimSupplier.cs
@@ -4,7 +4,6 @@ public record SlimSupplier
 {
     public string Code { get; init; } = string.Empty;
     public string Name { get; init; } = string.Empty;
-    public bool IsEnabled { get; init; }
     public EnablementState EnablementState { get; init; }
     public string ConnectorUrl { get; init; } = string.Empty;
     public string? ConnectorGrpcEndpoint { get; set; }
